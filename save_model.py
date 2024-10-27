004b33167fd6863d135844e2478dbdbb3bf55fae
Faisal Dad Khan <54923395+faisaldadkhan13@users.noreply.github.com>
2024-10-27 14:42:03 +0530

    Update save_model.py
    Signed-off-by: Faisal Dad Khan <54923395+faisaldadkhan13@users.noreply.github.com>

--- save_model.py
+++ save_model.py
@@ -2,11 +2,11 @@
 from model.luna_model import LunaAI
 from transformers import BertTokenizer
 
-def save_model(model):
-    model.save_pretrained('./luna_ai_model')
+def save_model(model, path='./luna_ai_model'):
+    model.save_pretrained(path)
     tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    tokenizer.save_pretrained('./luna_ai_model')
+    tokenizer.save_pretrained(path)
 
 if __name__ == "__main__":
-    model = LunaAI()
+    model = LunaAI(num_classes=2)  # Adjust num_classes if necessary
     save_model(model)
