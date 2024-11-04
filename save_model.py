8a3282edbdbd4b49c319fb505eafb17ef358131c
Faisal Dad Khan <54923395+faisaldadkhan13@users.noreply.github.com>
2024-11-04 08:17:27 +0400

    Update save_model.py
    Signed-off-by: Faisal Dad Khan <54923395+faisaldadkhan13@users.noreply.github.com>

--- save_model.py
+++ save_model.py
@@ -1,12 +1,6 @@
-# save_model.py
-from model.luna_model import LunaAI
-from transformers import BertTokenizer
+import pandas as pd
 
-def save_model(model, path='./luna_ai_model'):
-    model.save_pretrained(path)
-    tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
-    tokenizer.save_pretrained(path)
-
-if __name__ == "__main__":
-    model = LunaAI(num_classes=2)  # Adjust num_classes if necessary
-    save_model(model)
+def save_to_csv(text, label, file_path='data/data.csv'):
+    # Append new data to the CSV file
+    new_data = pd.DataFrame([[text, label]], columns=['text', 'label'])
+    new_data.to_csv(file_path, mode='a', header=False, index=False)
